7fce20743a36a48255e54a90833d2af44f83c4fc
denis <deniscgaetani@gmail.com>
2025-01-19 20:32:05 -0300

    Starting adding animations

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -1,17 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../models/meal.dart';
+import 'package:meals/models/meal.dart';
 
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((m) => m.id != meal.id).toList();
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 }
